24c923f251abdd735d319568cab2ae1ab5db015c
sakimyto <21049958+sakimyto@users.noreply.github.com>
2026-03-14 14:22:21 +0900

    feat: add GitHub GraphQL response types

---
+++ src/github/types.ts
@@ -0,0 +1,41 @@
+export interface GitHubUser {
+  login: string
+  repositories: {
+    nodes: GitHubRepo[]
+  }
+}
+
+export interface GitHubRepo {
+  name: string
+  pushedAt: string
+  defaultBranchRef: {
+    target: {
+      history: {
+        nodes: GitHubCommit[]
+        totalCount: number
+      }
+    }
+  } | null
+  claudeMd: FileCheck | null
+  agentsMd: FileCheck | null
+  cursorrules: FileCheck | null
+  cursorrulesDir: FileCheck | null
+  githubCopilot: FileCheck | null
+  claudeDir: FileCheck | null
+}
+
+interface FileCheck {
+  id: string
+}
+
+export interface GitHubCommit {
+  message: string
+  committedDate: string
+  author: {
+    user: { login: string } | null
+  }
+}
+
+export interface GitHubQueryResponse {
+  user: GitHubUser | null
+}
